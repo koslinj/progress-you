131e3cbcd1182a28d7f5c137ffd003d978c3361a
koslinj <koslinj@gmail.com>
2023-07-16 21:02:51 +0200

    check mark fix

--- src/components/Home/Day.tsx
+++ src/components/Home/Day.tsx
@@ -13,14 +13,14 @@ const Day = ({ item }: PropsType) => {
             <div>
                 <div className="flex justify-center gap-4 md:gap-8 items-center mt-4">
                     <img className="h-8 md:h-10" src={runIcon} alt="Run Icon" />
-                    {item.running ? <p>✔️</p> : <p>❌</p>}
+                    {item.running ? <p>✅</p> : <p>❌</p>}
                 </div>
                 {item.running && <p className='font-orbitron text-lg md:text-2xl'>{Math.floor(item.minutes.running/60)}:{String(item.minutes.running % 60).padStart(2,'0')}</p>}
             </div>
             <div>
                 <div className="flex justify-center gap-4 md:gap-8 items-center mt-4">
                     <img className="h-8 md:h-10" src={codeIcon} alt="Code Icon" />
-                    {item.coding ? <p>✔️</p> : <p>❌</p>}
+                    {item.coding ? <p>✅</p> : <p>❌</p>}
                 </div>
                 {item.coding && <p className='font-orbitron text-lg md:text-2xl'>{Math.floor(item.minutes.coding/60)}:{String(item.minutes.coding % 60).padStart(2,'0')}</p>}
             </div>
